95b091bae44e5193ae5107dc3af30b87b0405deb
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2022-06-21 00:46:57 +0200

    Add files via upload

---
+++ interact.js
@@ -0,0 +1,23 @@
+async function showCandidates(){
+    const abi = '[ { "inputs": [ { "internalType": "string", "name": "_name", "type": "string" } ], "name": "addCandidate", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "name": "candidates", "outputs": [ { "internalType": "uint256", "name": "id", "type": "uint256" }, { "internalType": "string", "name": "name", "type": "string" }, { "internalType": "uint256", "name": "voteCount", "type": "uint256" } ], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "candidatesCount", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" } ]'
+    const contractAddress = "0xC756ddE0C51233B3b7af734b8EE86302DBcC5B63"
+    
+    const signer = new ethers.Wallet('28d45e4da4cf34e4c2eebfc4f53653639a3011b5e1931ccf03d9151140005e9b', ethers.getDefaultProvider('ropsten'));
+    
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    cands = [];
+    const n= await contract.candidatesCount();   
+    console.log(n.toNumber());
+    for (let i = 1; i < n.toNumber()+1;  i++) {
+        a = await contract.candidates(i); 
+        cands.push(a['name']);
+    }
+    for (let i = 0; i < cands.length; i++) {
+        var ul = document.getElementById("list");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(cands[i]));
+        ul.appendChild(li); 
+    }
+
+}
